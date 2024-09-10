83fb84159f47d9411468e37e3b27e09f3d98ed9a
yifaaan <0219lyf@gmail.com>
2024-09-10 21:20:33 +0800

    support tower-http file serve

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -6,8 +6,8 @@ use axum::{
     routing::get,
     Router,
 };
+use tower_http::services::ServeDir;
 use tracing::{info, warn};
-
 #[derive(Debug)]
 struct HttpServeState {
     path: PathBuf,
@@ -17,9 +17,16 @@ pub async fn process_http_serve(path: PathBuf, port: u16) -> anyhow::Result<()> 
     let addr = SocketAddr::from_str(&format!("127.0.0.1:{}", port))?;
     info!("Serving {:?} on {}", path, addr);
 
-    let state = HttpServeState { path };
+    let state = HttpServeState { path: path.clone() };
+    let dir_service = ServeDir::new(path)
+        .append_index_html_on_directories(true)
+        .precompressed_gzip()
+        .precompressed_br()
+        .precompressed_deflate();
+
     let router = Router::new()
-        .route("/*path", get(file_handler))
+        // .route("/*path", get(file_handler))
+        .nest_service("/", dir_service)
         .with_state(Arc::new(state));
 
     let listener = tokio::net::TcpListener::bind(addr).await?;
